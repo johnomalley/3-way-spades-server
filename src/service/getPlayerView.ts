e12f2080af04c604809ff06209bb372ffabed524
John O'Malley <john.omalley@ocelotconsulting.com>
2020-05-09 17:37:03 -0500

    include phase in player view

--- src/service/getPlayerView.ts
+++ src/service/getPlayerView.ts
@@ -1,4 +1,4 @@
-import { Card, Game, Trick } from '../model/types'
+import { Card, Game, HandPhase, Trick } from '../model/types'
 import last = require('lodash/last')
 
 export type PlayerView = Readonly<{
@@ -6,6 +6,7 @@ export type PlayerView = Readonly<{
   endTime?: string
   timestamp: number
   playerNumber: number
+  phase: HandPhase
   currentPlayerNumber: number
   players: ReadonlyArray<{
     name: string
@@ -21,7 +22,7 @@ export type PlayerView = Readonly<{
 
 export default (game: Game, playerId: string): PlayerView => {
   const player = game.players.find(_ => _.id === playerId)!
-  const { currentPlayerNumber, playerHands, tricks } = last(game.hands)!
+  const { currentPlayerNumber, playerHands, tricks, phase } = last(game.hands)!
   const { cardsInHand, cardsPlayed } = playerHands[player.number]
   const [currentTrick, lastTrick] = tricks.slice(Math.max(0, tricks.length - 2)).reverse()
   return {
@@ -29,6 +30,7 @@ export default (game: Game, playerId: string): PlayerView => {
     endTime: game.endTime,
     timestamp: game.timestamp,
     playerNumber: player.number,
+    phase,
     currentPlayerNumber,
     players: game.players.map(({ name, number, points }) => ({
       name,
